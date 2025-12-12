8a3fe820ce5528245d5a22893479ecda0648ec76
User <User@DESKTOP-QHG18FL>
2025-12-12 21:05:19 +0300

    Single Window color DONE

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -233,6 +233,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		SetSkin(hwnd, "square_blue");
 	}
 	break;
+	case WM_CTLCOLOREDIT:
+	{
+		HDC hdc = (HDC)wParam;	//С сообщение WM_CTLCOLOREDIT в 'wParam' принимается HDC элемента EditControl
+		//SetBkMode(hdc, TRANSPARENT);	//Делаем фон hEdit непрозрачным.
+		SetBkColor(hdc, RGB(0, 0, 100));
+		SetTextColor(hdc, RGB(255, 0, 0));
+		HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 150));
+		SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)hBrush);
+		SendMessage(hwnd, WM_ERASEBKGND, wParam, 0);
+		return (LRESULT)hBrush;
+	}
+		break;
 	case WM_COMMAND:
 	{
 		static DOUBLE a = DBL_MIN, b = DBL_MIN;	//Операнды
