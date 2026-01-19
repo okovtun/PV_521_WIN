8a1eba5bc5b22627e0e3f395696d8bbea3dc2f5f
User <User@DESKTOP-QHG18FL>
2026-01-19 21:43:26 +0300

    Week days need improvement

--- WinForms/Clock/AlarmDialog.cs
+++ WinForms/Clock/AlarmDialog.cs
@@ -87,9 +87,16 @@ namespace Clock
 
 		private void buttonOK_Click(object sender, EventArgs e)
 		{
+			//if (clbWeekDays.CheckedIndices.Count == 0)
+			//{
+			//	MessageBox.Show(this, "Выберите хотябы один день недели", "Ну Ёжж..", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			//	return;
+			//}
+			//else this.buttonOK.DialogResult = DialogResult.OK;
 			Alarm.Date = checkBoxUseDate.Checked ? dtpDate.Value : DateTime.MaxValue;
 			Alarm.Time = dtpTime.Value.TimeOfDay;
-			Alarm.Days = new Week(checkBoxUseDate.Checked ? (byte)0 : GetDaysMask());
+			Alarm.Days = new Week(GetDaysMask());
+			//Alarm.Days = new Week(checkBoxUseDate.Checked ? (byte)0 : GetDaysMask());
 			Alarm.Filename = labelFilename.Text;
 		}
 	}
