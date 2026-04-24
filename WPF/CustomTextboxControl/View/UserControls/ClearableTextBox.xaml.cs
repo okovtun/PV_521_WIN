8625f47aee9f98ca9bc1948bfab7f09d3167fd2c
User <User@DESKTOP-QHG18FL>
2026-04-24 20:03:28 +0300

    ClearbleTextBox DONE

--- WPF/CustomTextboxControl/View/UserControls/ClearableTextBox.xaml.cs
+++ WPF/CustomTextboxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -20,14 +20,35 @@ namespace CustomTextboxControl.View.UserControls
 	/// </summary>
 	public partial class ClearableTextBox : UserControl
 	{
+		string placeholder;
+		public string Placeholder
+		{
+			get => placeholder;
+			set => placeholder = tbPlaceholder.Text = value;
+		}
 		public ClearableTextBox()
 		{
 			InitializeComponent();
 		}
 
-		private void btnClear_Click(object sender, RoutedEventArgs e)
-		{
+		private void btnClear_Click(object sender, RoutedEventArgs e) =>
 			txtInput.Text = "";
+
+		private void TxtInput_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				//Window window = Window.GetWindow(this);
+				e.Handled = true;
+				UIElement tb = sender as UIElement;
+				//tb?.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+				if (tb != null) tb.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+			}
+			if (e.Key == Key.Escape) btnClear_Click(sender, e);
 		}
+
+		private void TxtInput_TextChanged(object sender, TextChangedEventArgs e)=>
+			tbPlaceholder.Visibility = txtInput.Text == "" ? Visibility.Visible : Visibility.Hidden;
+		
 	}
 }
